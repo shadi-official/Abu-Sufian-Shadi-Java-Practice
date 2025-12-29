df339c04d06bf9bb4f03dc311e50a778de3db947
Abu Sufian Shadi <abusufianshadi01@gmail.com>
2025-12-29 23:24:55 +0600

    Create Task04.java

---
+++ Task04.java
@@ -0,0 +1,16 @@
+public class Task04 {
+    public static void main(String[] args) {
+        int[] numbers = {0, 1, 2, 4, 5};
+        int n = 5; // Array-r shobcheye boro shonkhya
+        int expectedSum = n * (n + 1) / 2;
+        
+        int actualSum = 0;
+        for (int num : numbers) {
+            actualSum += num;
+        }
+        
+        int missingNumber = expectedSum - actualSum;
+        
+        System.out.println("Output: " + missingNumber);
+    }
+}
