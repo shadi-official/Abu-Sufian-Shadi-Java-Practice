fe77dc3b41c133d58dcc38c6ae2d12e642bfed38
Abu Sufian Shadi <abusufianshadi01@gmail.com>
2025-12-30 00:12:51 +0600

    Task04.java

--- Task04.java
+++ Task04.java
@@ -1,7 +1,7 @@
 public class Task04 {
     public static void main(String[] args) {
         int[] numbers = {0, 1, 2, 4, 5};
-        int n = 5; // Array-r shobcheye boro shonkhya
+        int n = 5; 
         int expectedSum = n * (n + 1) / 2;
         
         int actualSum = 0;
