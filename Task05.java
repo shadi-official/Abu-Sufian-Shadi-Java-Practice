5847097e81d66ef0bbcf74bfb60d102e5b9d8f72
Abu Sufian Shadi <abusufianshadi01@gmail.com>
2025-12-30 22:20:04 +0600

    Task05.java

--- Task05.java
+++ Task05.java
@@ -1,25 +1,21 @@
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 public class Task05 {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 2, 3, 3, 3, 4, 4, 4, 4};
-        
-        Map<Integer, Integer> occurrences = new LinkedHashMap<>();
-        
-        for (int num : numbers) {
-            if (occurrences.containsKey(num)) {
-                
-                occurrences.put(num, occurrences.get(num) + 1);
-            } else {
-                
-                occurrences.put(num, 1);
+        for (int i = 0; i < numbers.length; i++) {
+            boolean alreadyDone = false;
+            for (int j = 0; j < i; j++) {
+                if (numbers[i] == numbers[j]) {
+                    alreadyDone = true;
+                    break;
+                }
             }
-        }
-        
-        
-        for (Map.Entry<Integer, Integer> entry : occurrences.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
+            if (!alreadyDone) {
+                int count = 0;
+                for (int num : numbers) {
+                    if (num == numbers[i]) count++;
+                }
+                System.out.println(numbers[i] + ": " + count);
+            }
         }
     }
 }
