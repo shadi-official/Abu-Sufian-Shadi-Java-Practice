3d7cf3fb172f87b2fe989381a766e31b6fe49fea
Abu Sufian Shadi <abusufianshadi01@gmail.com>
2025-12-30 22:17:38 +0600

    Task03.java

--- Task03.java
+++ Task03.java
@@ -1,20 +1,17 @@
-import java.util.HashSet;
-
 public class Task03 {
     public static void main(String[] args) {
         int[] numbers = {11, 7, 7, 11, 2, 15, 6, 6};
-        
-        
-        HashSet<Integer> uniqueSet = new HashSet<>();
         int sum = 0;
-        
-        for (int num : numbers) {
-            
-            if (uniqueSet.add(num)) {
-                sum += num;
+        for (int i = 0; i < numbers.length; i++) {
+            boolean isDuplicate = false;
+            for (int j = 0; j < i; j++) {
+                if (numbers[i] == numbers[j]) {
+                    isDuplicate = true;
+                    break;
+                }
             }
+            if (!isDuplicate) sum += numbers[i];
         }
-        
         System.out.println("Output: " + sum);
     }
 }
