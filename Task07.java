57b91a9b1bc4d2eba5c2f60d6c7f6163b05b2bfa
Abu Sufian Shadi <abusufianshadi01@gmail.com>
2025-12-30 22:20:45 +0600

    Task07.java

--- Task07.java
+++ Task07.java
@@ -1,23 +1,14 @@
-import java.util.HashSet;
-import java.util.Set;
-
 public class Task07 {
     public static void main(String[] args) {
         String input = "abcaabbcc";
-        
-        Set<Character> uniqueChars = new HashSet<>();
-        
-        for (char c : input.toCharArray()) {
-            uniqueChars.add(c);
+        String unique = "";
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (unique.indexOf(c) == -1) {
+                unique += c;
+            }
         }
-        
-        System.out.print("Unique characters: ");
-        int count = 0;
-        for (Character c : uniqueChars) {
-            System.out.print(c + (count < uniqueChars.size() - 1 ? ", " : ""));
-            count++;
-        }
-        
-        System.out.println("\nTotal unique characters: " + uniqueChars.size());
+        System.out.println("Unique characters: " + unique);
+        System.out.println("Total unique characters: " + unique.length());
     }
 }
